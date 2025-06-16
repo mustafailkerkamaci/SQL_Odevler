39c0dec7f9bb1a5cee7c54694fdd46209d873e3d
Mustafa İlker Kamaci <mustafailkerkmc@gmail.com>
2025-06-17 01:56:51 +0300

    Odev8.sql

--- Odev8.sql
+++ Odev8.sql
@@ -1,3 +1,4 @@
 DELETE FROM employee
 WHERE id IN (1, 2, 3, 4, 5);
 -- Update islemi ile eklenen id numaralari belirli olan kisiler silindi.
+-- Odev 8 icin yapilan islemleri goremk icin odev8 de yapilan islemlerin yazili oldugu readme dosyasina bakabilirsiniz.
